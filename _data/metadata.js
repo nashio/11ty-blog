59e235f530a0df991cef2082991904859c8365fe
Ignacio Chavez <nashio@Ignacios-MacBook-Pro.local>
2024-04-09 00:58:45 -0700

    updated metadata

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Ignacio Chavez",
+	url: "https://ignaciochavez.com/",
+	language: "en",
+	description: "random projects",
+	author: {
+		name: "Ignacio Chavez",
+		email: "tbd",
+		url: "https://ignaciochavez.com"
+	}
+}
